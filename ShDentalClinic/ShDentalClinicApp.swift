4008989fbe995c1dc5e01aaaea5890dd445e4b59
VitaliiShorin <9971425@mail.ru>
2025-05-06 16:18:12 +0300

    Initial Commit

---
+++ ShDentalClinic/ShDentalClinicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShDentalClinicApp.swift
+//  ShDentalClinic
+//
+//  Created by Виталий Шорин on 06.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShDentalClinicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
